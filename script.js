ab8e1a942443a06de10b5d7214611a95025f00cb
VioLet-Loutsik <55018769+VioLet-Loutsik@users.noreply.github.com>
2022-10-05 15:32:39 +0300

    homework

---
+++ script.js
@@ -0,0 +1,101 @@
+console.log(123)
+let number=345
+console.log(number)
+
+let first_num = 100
+let second_num = 200
+let result = (first_num+second_num)/2
+console.log(typeof result)
+
+let name = "Vanea"
+let age = 20
+let result_1 = name + ' ' + age + ' ' + "age" // конкатенация
+console.log(result_1)
+
+let result_2 = `${name} ${age} vanea`
+console.log(result_2);
+
+let word = 'file'
+word = 'word'
+console.log(word);
+
+let a = '2'
+a = Number(a)
+console.log(typeof a);
+
+let b = 30
+b = String(b)
+console.log(typeof b);
+
+// const age_2 = prompt('how old are you')
+// let result_3 = `You age ${age_2}`
+// console.log(result_3)
+
+
+
+// ДОМАШКА
+let nameHomeWork = 'ДОМАШКА'
+console.log(nameHomeWork);
+
+
+// 1
+let num_1 = 22
+console.log(num_1)
+// 2
+name= 'John'
+console.log(name);
+// 3
+let num_3 = 3
+console.log(num_3);
+
+// 4
+let a_4 = 10
+let b_4 = 2
+let c_4 = a_4 + b_4
+console.log(c_4);
+
+// 5
+let a_5 = 17
+let b_5 = 10
+let c_5 = b_5 - a_5
+console.log(c_5);
+// 6
+let str1 = 'Привет'
+let str2 = 'Мир'
+console.log(`'${str1} ${str2}!'`);
+//7
+let a_7 = 5
+let b_7 = 10
+let c_7 = a_7 + b_7
+console.log(c_7);
+//8
+let length = 16
+let lastName = "Johnson"
+console.log(typeof length);
+console.log(typeof lastName);
+
+// 9
+// let wether = prompt('Какая сейчас температура?')
+// let answer = `Температура на улице ${wether} градусов`
+// console.log(answer);
+
+// 10
+// let theater = prompt('Введите название кинотеатра')
+// let film = prompt('Введите название фильма')
+// let time = prompt('Введите время просмотра')
+// let answer =`Билет в кинотеатр ${theater} на фильм ${film} на время ${time} ЗАБРОНИРОВАН!`
+// console.log(answer);
+
+// 11
+// let a_11 =  Number(prompt('Введите любое число'))
+// let b_11 =  Number(prompt('Введите любое число'))
+// let c_11 =  Number(prompt('Введите любое число'))
+// let sum = a_11 + b_11 + c_11
+// console.log(`Сумма введенных чисел равна ${sum}`);
+
+// 12
+// let a_12 = Number(prompt('Введите вашу оценку'))
+// let b_12 = Number(prompt('Введите вашу оценку'))
+// let c_12 = Number(prompt('Введите вашу оценку'))
+// let result_12 = (a_12 + b_12 + c_12)/3
+// console.log(`Средний балл ученика составляет ${result_12}`);
